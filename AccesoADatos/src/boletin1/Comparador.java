adc053d463452762df638d237c0ce5dac11ec439
Alcel <pepeeldelaspapas@gmail.com>
2022-10-10 07:11:53 +0200

    Comit 7:11

--- AccesoADatos/src/boletin1/Comparador.java
+++ AccesoADatos/src/boletin1/Comparador.java
@@ -1,9 +1,18 @@
 package boletin1;
 
 public class Comparador {
-	
-
 	public static void main(String[] args) {
-		final String fichero1=".\\datos\\Fichero1.txt";
+		String sistemaOperativo = System.getProperty("os.name");
+		String fichero1;
+		String fichero2;
+		if(sistemaOperativo.startsWith("Win")) {
+			fichero1=".\\datos\\"+args[0]+".txt";
+			fichero2=".\\datos\\"+args[1]+".txt";
+			System.out.println("Hola");
+		}
+		else {
+			fichero1="./datos/"+args[0]+".txt";
+			fichero2="./datos/"+args[1]+".txt";
+		}
 	}
 }
