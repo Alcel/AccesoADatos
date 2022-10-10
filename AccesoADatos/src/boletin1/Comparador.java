d47b236b6b4381e43f52cc3873282951f7458fc1
Alcel <pepeeldelaspapas@gmail.com>
2022-10-10 07:27:59 +0200

    Comit 7:27

--- AccesoADatos/src/boletin1/Comparador.java
+++ AccesoADatos/src/boletin1/Comparador.java
@@ -1,14 +1,24 @@
 package boletin1;
-
+import java.io.File;
 public class Comparador {
 	public static void main(String[] args) {
 		String sistemaOperativo = System.getProperty("os.name");
 		String fichero1;
 		String fichero2;
+		File fFichero1 = null;
+		File fFichero2 = null;
 		if(sistemaOperativo.startsWith("Win")) {
 			fichero1=".\\datos\\"+args[0]+".txt";
 			fichero2=".\\datos\\"+args[1]+".txt";
+			fFichero1= new File(fichero1);
+			fFichero2= new File(fichero2);
 			System.out.println("Hola");
+			if(fFichero1.exists() && fFichero2.exists()) {
+				System.out.println("Ambos existen");
+			}
+			else {
+				System.err.println("Alguna de las rutas no existe");
+			}
 		}
 		else {
 			fichero1="./datos/"+args[0]+".txt";
