d0972143c057919567cc792686ab7b1f96fb29d6
Alcel <pepeeldelaspapas@gmail.com>
2022-10-19 11:40:50 +0200

    Jesus

--- AccesoADatos/src/boletin2/Plantilla.java
+++ AccesoADatos/src/boletin2/Plantilla.java
@@ -4,34 +4,62 @@ import java.io.*;
 
 public class Plantilla {
 	public static void main(String[] args) throws IOException {
-		nuevoEmpleado("Juan",23,1200,false);
+		//nuevoEmpleado("Ivan 'El ingles' Rodriguez",39,2200,true);
+		//muestraEmpleados();
+		muestraEmpleado(2);
 	}
 	
 	public static void nuevoEmpleado (String nombre, int edad, double sueldo, boolean jubilado) throws IOException {
 		File archivo = new File("./src/datos/plantilla.dat.txt");
 		archivo.createNewFile();
 		
-		FileOutputStream flujoOut = new FileOutputStream(archivo);
+		FileOutputStream flujoOut = new FileOutputStream(archivo, true);
+		
 		DataOutputStream dos = new DataOutputStream(flujoOut);
 		
-		dos.writeBytes(nombre);
-		dos.write(edad);
+		dos.writeUTF(nombre);
+		dos.writeInt(edad);
 		dos.writeDouble(sueldo);
 		dos.writeBoolean(jubilado);
-		
-		
 		
 		
 		dos.close();
 		flujoOut.close();
 	}
 	
-	public static void muestraEmpleados () {
+	public static void muestraEmpleados () throws IOException {
+		File archivo = new File("./src/datos/plantilla.dat.txt");
+		FileInputStream flujoIn = new FileInputStream(archivo);
+		DataInputStream dis = new DataInputStream(flujoIn); 
+	
+		while(dis.available()>0) {
 		
+		System.out.print(dis.readUTF()); 
+		System.out.print(dis.readInt()); 
+		System.out.print(dis.readDouble());
+		System.out.println(dis.readBoolean()); 
+		}
+		dis.close();
+		flujoIn.close();
 	}
 	
-	public static void muestraEmpleado (int orden) {
-		
+	public static void muestraEmpleado (int orden) throws IOException {
+		File archivo = new File("./src/datos/plantilla.dat.txt");
+		FileInputStream flujoIn = new FileInputStream(archivo);
+		DataInputStream dis = new DataInputStream(flujoIn); 
+		for(int i=0;i!=orden;i++) {
+			dis.readUTF();
+			dis.readInt();
+			dis.readDouble();
+			dis.readBoolean();
+		}
+
+		System.out.print(dis.readUTF()); 
+		System.out.print(dis.readInt()); 
+		System.out.print(dis.readDouble());
+		System.out.println(dis.readBoolean()); 
+		dis.close();
+		flujoIn.close();
 	}
 	
 	public static void eliminaEmpleado(int orden) {
