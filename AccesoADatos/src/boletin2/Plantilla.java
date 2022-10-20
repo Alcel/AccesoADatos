09afd4924dce95150c9456641006687044c83fad
Alcel <pepeeldelaspapas@gmail.com>
2022-10-20 12:39:36 +0200

    Update

--- AccesoADatos/src/boletin2/Plantilla.java
+++ AccesoADatos/src/boletin2/Plantilla.java
@@ -1,16 +1,18 @@
 package boletin2;
 
 import java.io.*;
+import java.util.ArrayList;
 
 public class Plantilla {
 	public static void main(String[] args) throws IOException {
 		//nuevoEmpleado("Ivan 'El ingles' Rodriguez",39,2200,true);
 		//muestraEmpleados();
-		muestraEmpleado(2);
+		//muestraEmpleado(0);
+		eliminaEmpleado(0);
 	}
 	
 	public static void nuevoEmpleado (String nombre, int edad, double sueldo, boolean jubilado) throws IOException {
-		File archivo = new File("./src/datos/plantilla.dat.txt");
+		File archivo = new File("./src/datos/plantilla.dat");
 		archivo.createNewFile();
 		
 		FileOutputStream flujoOut = new FileOutputStream(archivo, true);
@@ -28,7 +30,7 @@ public class Plantilla {
 	}
 	
 	public static void muestraEmpleados () throws IOException {
-		File archivo = new File("./src/datos/plantilla.dat.txt");
+		File archivo = new File("./src/datos/plantilla.dat");
 		FileInputStream flujoIn = new FileInputStream(archivo);
 		DataInputStream dis = new DataInputStream(flujoIn); 
 	
@@ -44,7 +46,7 @@ public class Plantilla {
 	}
 	
 	public static void muestraEmpleado (int orden) throws IOException {
-		File archivo = new File("./src/datos/plantilla.dat.txt");
+		File archivo = new File("./src/datos/plantilla.dat");
 		FileInputStream flujoIn = new FileInputStream(archivo);
 		DataInputStream dis = new DataInputStream(flujoIn); 
 		for(int i=0;i!=orden;i++) {
@@ -62,7 +64,28 @@ public class Plantilla {
 		flujoIn.close();
 	}
 	
-	public static void eliminaEmpleado(int orden) {
+	public static void eliminaEmpleado(int orden) throws IOException {
+		/*Array y no añadir de vuelta*/
+		File archivo = new File("./src/datos/plantilla.dat");
+		FileInputStream flujoIn = new FileInputStream(archivo);
+		DataInputStream dis = new DataInputStream(flujoIn); 
+		ArrayList<String> nombre = new ArrayList<String>();
+		ArrayList<Integer> edad = new ArrayList<Integer>();
+		ArrayList<Double> sueldo = new ArrayList<Double>();
+		ArrayList<Boolean> jubilado = new ArrayList<Boolean>();
+	
+		while(dis.available()>0) {
+		nombre.add(dis.readUTF());
+		edad.add(dis.readInt());
+		sueldo.add(dis.readDouble());
+		jubilado.add(dis.readBoolean());
+		
+		}
+		
+		System.out.println(nombre);
+		dis.close();
+		flujoIn.close();
+		
 		
 	}
 
